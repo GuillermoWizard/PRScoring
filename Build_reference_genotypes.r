53061aa70f73f1707c24797051144b8a5c5d0323
GuillermoWizard <Neowily@gmail.com>
2021-07-05 10:33:23 -0500

    Build Refeence Genotypes for PR Normalization

--- Build_reference_genotypes.r
+++ Build_reference_genotypes.r
@@ -1,19 +1,29 @@
 #!/usr/bin/env Rscript
-#install.packages("vcfR")
 args = commandArgs(trailingOnly=TRUE)
 stfu = suppressPackageStartupMessages
+defaultW <- getOption("warn") 
+options(warn = -1)
 stfu(library(crayon))
 stfu(library(data.table))
 stfu(library(stringr))
 stfu(library(tidyverse))
 stfu(library(vcfR))
-
 green = make_style(rgb(red=0,green=250,blue=0,maxColorValue=255))
+options(warn = defaultW)
 
+helpmessage="
+  Build_Reference_Genotypes.R [Assoc File] [VCF with samples genotypes]
+    Inputs:
+      [Assoc]   Tab separated file with (tsv extension) with summary statistics from GWAS in PRCSice format. 
+      [VCF]     VCF file (vcf extension) with genotype data from sample individuals.      
+  
+  "
 
 if (length(args)==0) {
-  stop("At least one argument must be supplied (final report file, associacion table, variants table, barcode ).", call.=FALSE)
+  cat(helpmessage)
+  stop("At least one argument must be supplied (assoc, vcf ).", call.=FALSE)
 } else if (length(args)==1) {
+  cat(helpmessage)
   stop("At least two arguments/files must be supplied.", call.=FALSE)
 } else if (length(args)==2) {
   file1=args[1];
@@ -22,6 +32,9 @@ if (length(args)==0) {
 
 
 main <- function(AssocFile, vcfFile){
+  defaultW <- getOption("warn") 
+  options(warn = -1)
+  
   Assocdata = as.data.frame(fread(AssocFile,header=T))
   vcf <- read.vcfR(vcfFile, verbose = FALSE )
   
@@ -102,18 +115,18 @@ main <- function(AssocFile, vcfFile){
   validcols=c(colnamesvcf,colnamesgenotypes,c("AFECTADO1","AFECTADO2","NEUTRO","PROTEGIDO1","PROTEGIDO2"))
   new_vcf = vcf_simulated %>% select(all_of(validcols))
   colnames(new_vcf)[1] = paste0("#",validcols[1])
-  #head(new_vcf)
   outputfile<-rename_output(file2)
   writeLines(meta, outputfile)
-  #write.table(metadata, file = outputfile, sep = "\t",col.names = TRUE,row.names=FALSE,quote = FALSE)
   write.table(new_vcf, file = outputfile, sep = "\t",col.names = TRUE,row.names=FALSE,quote = FALSE,append = TRUE)
-  message("\n\n",yellow$underline$bold(paste("Writing output at:",outputfile)))
+  options(warn = defaultW)
+  cat(outputfile)
   
 }
 
+
 rename_output <- function(File){
   executiondate=format(Sys.time(), "%Y%m%d")
-  sufix=gsub(' ','',paste(".risk_references.",executiondate,".vcf",collapser=""))
+  sufix=gsub(' ','',paste(".Reference.vcf",collapser=""))
   Outputfilename=gsub(".vcf",sufix, File)
   return(Outputfilename)
 }
@@ -124,12 +137,14 @@ if (!interactive()){
   arg=commandArgs(TRUE)
   if(length(arg)!=0){
     tm = system.time(main(file1,file2))
-    message("\n Running time: ")
-    print(tm)
-    message("Run complete\t",green("!"))
+    #message("\n Running time: ")
+    #print(tm)
+    #message("Run complete\t",green("!"))
   } else {
     #display_help()
   }
 } else {
-  message("Sourcing ...")
+  #message("Sourcing ...")
 }
+
+
